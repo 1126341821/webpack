69ec7e156aca0398faee253758f36f589db91c12
wb-tyz279201 <wb-tyz279201@alibaba-inc.com>
2017-10-20 18:07:49 +0800

    tyz

--- app/Greeter.js
+++ app/Greeter.js
@@ -4,14 +4,47 @@ import React, {
 } from 'react'
 import config from './config.json';
 import styles from './Greeter.scss'; //导入
-console.log(config)
 
 class Greeter extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: ''
+		};
+	}
+	request(url, method) {
+		fetch(url, {
+			method: method,
+			mode: "no-cors",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			}
+		}).then(function(res) {
+			console.log("Response succeeded?", JSON.stringify(res.ok));
+			console.log(JSON.stringify(res));
+		}).catch(function(e) {
+			console.log("fetch fail", JSON.stringify(params));
+		});
+	}
+	jsonpCallback(result) {
+		this.data = result;
+	}
+	click() {
+		const url = "http://localhost:8081/tao";
+		this.request(url, "GET");
+	}
 	render() {
-		return(
-			<div className={styles.root} >//添加类名
-		        {config.greetText}
-		      </div>
+			const {
+				value
+			} = this.state;
+			console.log(this.state);
+			return(
+					<div>
+				<div className={styles.root} onClick={this.click.bind(this)}>//添加类名
+			        {config.greetText}
+			    </div> 
+ <div >{value} < /div>
+			< /div>
 		);
 	}
 }
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+app.get('/tao/:id/:password', function(req, res) {  
+    res.header("Access-Control-Allow-Origin", "*");   //设置跨域访问 
+    const end=JSON.stringify({id:req.params.id, name: req.params.password})
+    res.jsonp(end)
+});  
+app.use(express.static('build'));
+
+//app.get('/tao', function(req, res) {
+//	res.send('Hello World');
+//})
+
+var server = app.listen(8081, function() {
+
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})

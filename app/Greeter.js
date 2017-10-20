84a6dc7c397d217482f4aae02316047f4925e079
wb-tyz279201 <wb-tyz279201@alibaba-inc.com>
2017-10-20 10:39:44 +0800

    tyz

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,17 +1,19 @@
 //Greeter,js
-import React, {Component} from 'react'
+import React, {
+	Component
+} from 'react'
 import config from './config.json';
-import styles from './Greeter.scss';//导入
-console.log(styles)
+import styles from './Greeter.scss'; //导入
+console.log(config)
 
-class Greeter extends Component{
-  render() {
-    return (
-       <div className={styles.root}>//添加类名
-        {config.greetText}
-      </div>
-    );
-  }
+class Greeter extends Component {
+	render() {
+		return(
+			<div className={styles.root} >//添加类名
+		        {config.greetText}
+		      </div>
+		);
+	}
 }
 
 export default Greeter
